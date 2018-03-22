298aaac2a41cb853fb70f4c0ef909aeb3f8b4449
dianjuar <dianjuar@gmail.com>
2018-03-21 23:10:57 -0500

    Create api-user and local-user components
    Related #1

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
+import { ApiUsersComponent } from './api-users/api-users.component';
+import { LocalUsersComponent } from './local-users/local-users.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UsersComponent]
+  declarations: [UsersComponent, ApiUsersComponent, LocalUsersComponent]
 })
 export class UsersModule { }
