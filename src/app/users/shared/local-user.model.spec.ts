2715b3dd0e01aacd117382d0f7245ea8e0bf5a89
dianjuar <dianjuar@gmail.com>
2018-03-22 20:43:52 -0500

    Create the model of local user
    related #3

---
+++ src/app/users/shared/local-user.model.spec.ts
@@ -0,0 +1,7 @@
+import { LocalUser } from './local-user.model';
+
+describe('LocalUser', () => {
+  it('should create an instance', () => {
+    expect(new LocalUser()).toBeTruthy();
+  });
+});
---
+++ src/app/users/shared/local-user.model.ts
@@ -0,0 +1,87 @@
+/**
+ *  Model of the local user
+ */
+export class LocalUser {
+
+  /**
+   * First name of the user
+   */
+  firstName: string;
+
+  /**
+   * last name of the user
+   */
+  lastName: string;
+
+  /**
+   * Email of the user
+   */
+  email: string;
+
+  /**
+   * Phone of the user
+   */
+  phone: string;
+
+  /**
+   * Birth date of the user
+   */
+  birthDate: Date;
+
+  /**
+   * The age of the user
+   */
+  age: number;
+
+  constructor(
+    firstName: string,
+    lastName: string,
+    email: string,
+    phone: string,
+    birthDate: Date,
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phone = phone;
+    this.birthDate = birthDate;
+
+    // Calculate the age
+    this.setAge();
+  }
+
+  /**
+   * Construct an empty local user object
+   *
+   * @returns {LocalUser} the empty user
+   */
+  static initEmptyUser() {
+    return new LocalUser('', '', '', '', null);
+  }
+
+  /**
+   * Given a date calculate the years until now
+   *
+   * @param birthDate The date to now the years until now
+   * @returns {number} The years between the given date an now
+   */
+  static calculateYears(birthDate: Date) {
+    // Time difference
+    const timeDiff = Math.abs(new Date().getTime() - birthDate.getTime());
+    // Days difference
+    const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    // Transform the days on years
+    return Math.trunc(diffDays / 365);
+  }
+
+  /**
+   * Calculate the age an put it to the attribute
+   */
+  setAge() {
+    if (!!this.birthDate) {
+      this.age = LocalUser.calculateYears(this.birthDate);
+    }
+  }
+
+}
