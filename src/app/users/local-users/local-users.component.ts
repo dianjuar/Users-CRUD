b73f08e7412fbb0a0a294f96b9d9e761b2c4bbe0
dianjuar <dianjuar@gmail.com>
2018-03-23 12:51:59 -0500

    Load local users from local storage
    Related #5

--- src/app/users/local-users/local-users.component.ts
+++ src/app/users/local-users/local-users.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LocalUserService } from '../shared/local-user.service';
+import { LocalUser } from '../shared/local-user.model';
+
 @Component({
   selector: 'app-local-users',
   templateUrl: './local-users.component.html',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocalUsersComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * Reference for user list that is on the LocalUserService
+   */
+  users: Array<LocalUser>;
+
+  /**
+   * To indicate if the component is loading
+   */
+  loading;
+
+
+  constructor(
+    private localUserService: LocalUserService
+  ) {
+    // We are loading
+    this.loading = true;
+
+    this.localUserService.isLoading()
+      .subscribe(() => this.loading = false);
+  }
 
   ngOnInit() {
+    this.users = this.localUserService.users;
   }
 
 }
